8c31aa7c8de8200cdfa1b9912479f3c4ad0ecca4
Tássis Fernando <tassis.fernandes@dtidigital.com.br>
2022-11-14 17:12:41 -0300

    Refatorando comentarios

--- node_app/app/index.js
+++ node_app/app/index.js
@@ -1,35 +1,37 @@
 const net = require('net');
 const prompt = require('prompt-sync')({sigint: true});
 
-const HOST = '127.0.0.1'; // parametrizar o IP do Listen
-const PORT = 6969; // porta TCP LISTEN
+const HOST = '127.0.0.1'; // IP do nosso host (local)
+const PORT = 6969; // definindo a porta
 
 // Cria a instância do Server e aguarda uma conexão
 net.createServer(sock => {
     sock.setEncoding('utf8');
 
-    // Opa, recebemos uma conexão - um objeto socket é associado à conexão automaticamente
+    // Ao receber uma nova conexão um objeto socket é associado à conexão automaticamente
     console.log(`USUÁRIO CONECTADO: ${sock.remoteAddress}:${sock.remotePort}`);
 
-    // Adiciona um 'data' - "event handler" nesta instância do socket
+    // Evento que recebe mensagens de usuários
     sock.on('data', data => {
         // dados foram recebidos no socket
-        // Escreve a mensagem recebida de volta para o socket (echo)
         console.log(`> ${data}`);
       
+        // faz a leitura de mensagem via terminal
         const message = prompt('> Envie a sua mensagem: ');
         
+        // envia a mensagem de resposta
         const buffer = Buffer.from(message);
         sock.setNoDelay(true);
         sock.write(buffer.toString()+'\r\n');
     });
 
-    // Adiciona um 'close' - "event handler" nesta instância do socket
+    // Evento close, que fecha a conexão
     sock.on('close', () => {
         // conexão fechada
         console.log(`CLOSED: ${sock.remoteAddress}:${sock.remotePort}`);
     });
 
+    // Evento end, que finaliza o servidor
     sock.on('end', () => {
         console.log('Servidor finalizado');
     });
